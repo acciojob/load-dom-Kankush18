848b464e5bdeadb3acf4811f86c9f83128b3f949
ANKUSH KUMAR <77243849+Kankush18@users.noreply.github.com>
2024-02-28 16:08:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function() {
-  // Get the body element
+
   var body = document.querySelector("body");
 
-  // Check if body is empty
+  
   if (body.innerHTML.trim() === "") {
-    // If body is empty, write the string "DOM load success"
+   
     body.innerText = "DOM load success";
   }
 });
