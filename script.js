cc6bfbe22625430dfc62b2457fa909512e49ddaf
ANKUSH KUMAR <77243849+Kankush18@users.noreply.github.com>
2024-02-28 16:20:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 //your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener('DOMContentLoaded', function() {
 
   var body = document.querySelector("body");
 
   
-  if (body.innerHTML.trim() === "") {
+  
    
     body.innerText = "DOM load success";
-  }
+  
 });
